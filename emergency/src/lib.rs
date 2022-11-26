d42c2d15ca0d32ea879f8760070c053d7e596773
wetalice <wetalice@protonmail.com>
2022-11-26 18:09:16 +0800

    add storages and funcs for emergency module

--- emergency/src/lib.rs
+++ emergency/src/lib.rs
@@ -20,6 +20,9 @@
 /// <https://docs.substrate.io/v3/runtime/frame>
 pub use pallet::*;
 use dao;
+use frame_support::codec::{Decode, Encode};
+use sp_runtime::RuntimeDebug;
+use scale_info::TypeInfo;
 
 #[cfg(test)]
 mod mock;
@@ -30,10 +33,25 @@ mod tests;
 #[cfg(feature = "runtime-benchmarks")]
 mod benchmarking;
 
+
+#[derive(PartialEq, Eq, Clone, RuntimeDebug, Encode, Decode, TypeInfo)]
+pub struct ProposalInfo<AccountId, Hash, Amount, BlockNumber> {
+	who: Option<AccountId>,
+	end_block: BlockNumber,
+	proposal_hash: Hash,
+	enact_period: BlockNumber,
+	pledge: Amount,
+	reason: Vec<u8>,
+}
+
+
 #[frame_support::pallet]
 pub mod pallet {
 	use frame_support::{dispatch::DispatchResultWithPostInfo, pallet_prelude::*, traits::Currency};
 	use frame_system::pallet_prelude::*;
+	use sp_runtime::biguint::Double;
+	use dao::Daos;
+	use crate::ProposalInfo;
 
 	pub type BalanceOf<T> =
 	<<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
@@ -48,7 +66,7 @@ pub mod pallet {
 
 		type Currency: Currency<Self::AccountId>;
 
-		type Pledge: Get<BalanceOf<Self>>;
+		type MinPledge: Get<BalanceOf<Self>>;
 
 	}
 
@@ -65,13 +83,22 @@ pub mod pallet {
 	// https://docs.substrate.io/v3/runtime/storage#declaring-storage-items
 	pub type Something<T> = StorageValue<_, u32>;
 
-	// #[pallet::storage]
-	// #[pallet::getter(fn members)]
-	// pub type Members<T> = StorageMap<_, Identity, >
-
 	#[pallet::storage]
 	#[pallet::getter(fn members)]
 	pub type Members<T: Config> = StorageMap<_, Identity, <T as dao::Config>::DaoId, Vec<T::AccountId>, ValueQuery>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn pledge_of)]
+	pub type PledgeOf<T: Config> = StorageMap<_, Identity, T::DaoId, BalanceOf<T>, ValueQuery>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn hashes_of)]
+	pub type HashesOf<T: Config> = StorageMap<_, Identity, T::DaoId, Vec<T::Hash>, ValueQuery>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn proposal_of)]
+	pub type ProposalOf<T: Config> = StorageDoubleMap<_, Identity, T::DaoId, Identity, T::Hash, ProposalInfo<T::AccountId, T::Hash, BalanceOf<T>, T::BlockNumber>>;
+
 
 	// Pallets use events to inform users when important changes are made.
 	// https://docs.substrate.io/v3/runtime/events-and-errors
@@ -102,11 +129,30 @@ pub mod pallet {
 	impl<T: Config> Pallet<T> {
 
 		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
-		pub fn set_memers(origin: OriginFor<T>, members: Vec<T::AccountId>) -> DispatchResultWithPostInfo {
+		pub fn set_memers(origin: OriginFor<T>, dao_id: T::DaoId, members: Vec<T::AccountId>) -> DispatchResultWithPostInfo {
 			Ok(().into())
 		}
 
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+		pub fn external_track(origin: OriginFor<T>, dao_id: T::DaoId, proposal: <T as dao::Config>::DaoId, reason: Vec<u8>) -> DispatchResultWithPostInfo {
+			T::ExternalOrigin::ensure_origin(origin)?;
+			Ok(().into())
+		}
 
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+		pub fn internal_track(origin: OriginFor<T>, dao_id: T::DaoId, proposal: <T as dao::Config>::DaoId, reason: Vec<u8>) -> DispatchResultWithPostInfo {
+			Ok(().into())
+		}
+
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+		pub fn reject(origin: OriginFor<T>, dao_id: T::DaoId, proposal_hash: T::Hash) -> DispatchResultWithPostInfo {
+			Ok(().into())
+		}
+
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+		pub fn enact_proposal(origin: OriginFor<T>, dao_id: T::DaoId, proposal_hash: T::Hash) -> DispatchResultWithPostInfo {
+			Ok(().into())
+		}
 
 	}
 }
